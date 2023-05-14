78b4af3e7457a5f49d645a25efd4684d8a892b03
Wuju03 <softabc12@gmail.com>
2023-05-15 00:16:19 +0900

    ㅇㅇ

--- etc/array_homework2.cpp
+++ etc/array_homework2.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 int main(){
     int status; // 입금, 출금, 종료 구분해주는 변수
-    int ammount, sum = 100000; // ammount는 입출금 금액, sum은 총금액
+    int ammount, account = 100000; // ammount는 입출금 금액, sum은 총금액
     
     do
     {
@@ -12,13 +12,13 @@ int main(){
         case 0:
             scanf("%d", &ammount);
             printf("입금 %d원\n", ammount);
-            sum += ammount;
+            account += ammount;
             break;
         
         case 1:
             scanf("%d", &ammount);
             printf("출금 %d원\n", ammount);
-            sum -= ammount;
+            account -= ammount;
             break;
 
         case -1:
@@ -27,7 +27,7 @@ int main(){
         }
     } while (status+1); // status가 0, 1일때 진행하고, -1일때 멈춰야하므로
     
-    printf("현재 잔액은 : %d원입니다.", sum);
+    printf("현재 잔액은 : %d원입니다.", account);
 
     return 0;
 }
